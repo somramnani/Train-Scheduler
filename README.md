178de585dd2d178d79c111a61f4a040a8c15c5b6
Som Ramnani <som.ramnani@gmail.com>
2019-04-02 17:41:49 -0500

    added comments and edited README file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Train-Scheduler
+A train schedule application that incorporates Firebase to host arrival and departure data. The app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+##Languages/Tools Used
+- HTML5
+- CSS3
+- Bootstrap
+- JavaScript
+- JQuery
+- Firebase
+
+##Features
+- When adding trains, users are able to submit the following:
+  - Train Name
+  - Destination
+  - First Train Time -- in military time
+  - Frequency -- in minutes
+
+- This app calculates when the next train will arrive; this should    be relative to the current time.
+
+- Users from many different machines are able to view same train      times.
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,76 @@
 //__________________________________________________________
 //GLOBAL VARIABLES
 //__________________________________________________________
-	
 
+//Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBZvgarykUuLI_PI_XYyX2YRaY625Y8brw",
+  authDomain: "train-scheduler-579d9.firebaseapp.com",
+  databaseURL: "https://train-scheduler-579d9.firebaseio.com",
+  projectId: "train-scheduler-579d9",
+  storageBucket: "train-scheduler-579d9.appspot.com",
+  messagingSenderId: "809459230620"
+};
 
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#add-user").on("click", function(event) {
+  // stops it from submitting to default
+  event.preventDefault();
+
+  // Saves the inputs in a variable
+  var name = $("#name-input").val()
+  var destination = $("#destination-input").val()
+  var time = $("#time-input").val().trim();
+  var frequency = $("#frequency-input").val()
+
+  // Puts the information in the Firebase Database
+  database.ref().push({
+    Train_Name: name,
+    Desination: destination,
+    Time: time,
+    frequency: frequency,
+  })
+
+  // Clears all of the text-boxes
+  $("#employee-name-input").val("");
+  $("#role-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+})
+
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  alert("A new worker has been added!");
+  
+  // Saves the value from the Database in a variable
+  var newTrain = snapshot.val().name;
+  var newDestination = snapshot.val().destination;
+  var newTime = snapshot.val().time;
+  var newFrequency = snapshot.val().frequency;
+
+  // Viewing the saved values to the console
+  console.log(newTrain);
+  console.log(newDestination);
+  console.log(newTime);
+  console.log(newFrequency);
+
+  // Creating a new row to save and display the information that is saved to the database
+  var newRow = $("<tr>")
+  var newTrainRow = "<th scope='row'>" + newTrain + "</th>";
+  var newDestinationRow = "<td>" + newDestination + "</td>";
+  var newTimeRow = "<td>" + newTime + "</td>";
+  var newFrequencyRow = "<td>$" + newFrequency + "</td>";
+
+  // Adds the information to the row
+  newRow.append(newTrainRow);
+  newRow.append(newDestinationRow);
+  newRow.append(newTimeRow);
+  newRow.append(newFrequencyRow);
+  
+  // Adds the row to the HTML page
+  $("#table-body").append(newRow)
+})
