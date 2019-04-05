fdd7668e38ef6b214b2b91f20bd9743de7921ea3
Som Ramnani <som.ramnani@gmail.com>
2019-04-05 15:55:18 -0500

    made the info push to the table, added moment for minutes away & added a digital clock

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,8 +16,20 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+
+function timer(){
+  //Current Time
+  var currentTime = moment();  
+  var time = moment(currentTime).format("hh:mm:ss");
+  // Puts the current time under the title
+  $("#time").text("Current Time: " + time);
+}
+
+setInterval(timer, 1000);
+
 $("#add-user").on("click", function(event) {
-  // stops it from submitting to default
+
+  // Stops it from submitting to default
   event.preventDefault();
 
   // Saves the inputs in a variable
@@ -28,9 +40,9 @@ $("#add-user").on("click", function(event) {
 
   // Puts the information in the Firebase Database
   database.ref().push({
-    Train_Name: name,
-    Desination: destination,
-    Time: time,
+    name: name,
+    destination: destination,
+    time: time,
     frequency: frequency,
   })
 
@@ -44,7 +56,7 @@ $("#add-user").on("click", function(event) {
 database.ref().on("child_added", function(snapshot) {
   console.log(snapshot.val());
 
-  alert("A new worker has been added!");
+  alert("A new train has been added!");
   
   // Saves the value from the Database in a variable
   var newTrain = snapshot.val().name;
@@ -52,24 +64,37 @@ database.ref().on("child_added", function(snapshot) {
   var newTime = snapshot.val().time;
   var newFrequency = snapshot.val().frequency;
 
-  // Viewing the saved values to the console
-  console.log(newTrain);
-  console.log(newDestination);
-  console.log(newTime);
-  console.log(newFrequency);
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(newTime, "HH:mm").subtract(1, "years");
+
+ // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+ 
+
+  //Time apart (remainder)
+  var tRemainder = diffTime % newFrequency;
+
+  // Minute Until Train
+  var tMinutesTillTrain =  newFrequency - tRemainder;
+
+
+  
 
   // Creating a new row to save and display the information that is saved to the database
   var newRow = $("<tr>")
   var newTrainRow = "<th scope='row'>" + newTrain + "</th>";
   var newDestinationRow = "<td>" + newDestination + "</td>";
   var newTimeRow = "<td>" + newTime + "</td>";
-  var newFrequencyRow = "<td>$" + newFrequency + "</td>";
+  var newFrequencyRow = "<td>" + newFrequency + "</td>";
+  var arrivalTimeRow = "<td>" + tMinutesTillTrain + "</td>";
 
   // Adds the information to the row
   newRow.append(newTrainRow);
   newRow.append(newDestinationRow);
-  newRow.append(newTimeRow);
   newRow.append(newFrequencyRow);
+  newRow.append(newTimeRow);
+  newRow.append(arrivalTimeRow);
   
   // Adds the row to the HTML page
   $("#table-body").append(newRow)
